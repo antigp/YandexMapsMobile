f93aac21c313f3fd08e985971e0ab39b1d3f6af8
Eugene Antropov <antigp@gmail.com>
2021-04-17 16:07:23 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
   targets: [
     .binaryTarget(
       name: "YandexMapsMobile",
-      url: "http://antropov.s3.amazonaws.com/public/YandexMapsMobile.xcframework.zip",
+      url: "https://antropov.s3.amazonaws.com/public/YandexMapsMobile.xcframework.zip",
       checksum: "e41fb37dd4777eab17189c6a25c11fe183ef870f6bea37429b25006767f74bc5"
     ),
   ]
